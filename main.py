320ec1ee51f21007792b863848f5b473aa274692
douglasfariasil <douglasfariasil@outlook.com>
2025-05-14 11:17:51 -0300

    Adicionar mais um arquivo.

---
+++ main.py
@@ -0,0 +1,17 @@
+# Definição do menu visualmente um pouco melhor
+menu = """
+================ MENU ================
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+======================================
+=> """
+
+# Variáveis iniciais com nomes mais descritivos e tipos apropriados
+saldo_conta = 0.0
+limite_por_saque = 500.0
+extrato_transacoes = []  # Usar uma lista para o extrato
+numero_saques_realizados = 0
+MAX_SAQUES_DIARIOS = 3
+
